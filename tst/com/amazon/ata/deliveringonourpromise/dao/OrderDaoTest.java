fa2e774285fe3ef6462b4dcbd9c046a97f579011
Andrew Lam <andrew.x.li.lam@gmail.com>
2022-07-21 21:58:43 -0500

    checksytle and Javacoco fixes

--- tst/com/amazon/ata/deliveringonourpromise/dao/OrderDaoTest.java
+++ tst/com/amazon/ata/deliveringonourpromise/dao/OrderDaoTest.java
@@ -2,20 +2,7 @@ package com.amazon.ata.deliveringonourpromise.dao;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class OrderDaoTest {
 
     private OrderDao dao;
-
-    @Test
-    public void get_forKnownOrderId_returnsOrder() {
-        // GIVEN
-        String ValidOrderID = "900-3746401-0000001";
-
-        //WHEN
-        //THEN
-        assertEquals(null, dao.get("900-3746401-0000001"), "Expected Not null got: ");
-
-    }
 }
